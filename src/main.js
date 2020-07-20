ad4b13aedd2a22a27324d2205588dbbb603f5104
ZhaiXiaoyuan <2549004118@qq.com>
2020-07-20 23:08:09 +0800

    添加浏览器环境判断

--- src/main.js
+++ src/main.js
@@ -29,22 +29,22 @@ Vue.use(api);
 Vue.use(tools);
 Vue.use(components);
 
+function isIE() { //ie?
+    if (!!window.ActiveXObject || "ActiveXObject" in window)
+        return true;
+    else
+        return false;
+}
 
 //使用钩子函数对路由进行权限跳转
 router.beforeEach((to, from, next) => {
-    if(to.meta.permission){
-        const role = localStorage.getItem('ms_username');
-        // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
-        role === 'admin' ? next() : next('/login');
+    //临时测试
+    if(false&&isIE()){
+        Vue.prototype.$alert('为了更好的使用体验，请使用谷歌浏览器、QQ浏览器、搜狗浏览器、360浏览器（极速模式）等浏览器打开网站', '温馨提示', {
+            confirmButtonText: '确定'
+        });
     }else{
-        // 简单的判断IE10及以下不进入富文本编辑器，该组件不兼容
-        if(navigator.userAgent.indexOf('MSIE') > -1 && to.path === '/editor'){
-            Vue.prototype.$alert('vue-quill-editor组件不兼容IE10及以下浏览器，请使用更高版本的浏览器查看', '浏览器不兼容通知', {
-                confirmButtonText: '确定'
-            });
-        }else{
-            next();
-        }
+        next();
     }
 })
 
